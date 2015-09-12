217d32a84af31f5b4b82e31909199e5766de8085
Murali GS <murali.gs@cognizant.com>
2015-09-12 18:48:04 +0530

    fixed failing test cases in booking spec model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ActiveRecord::Base
-	 belongs_to :user
-	 belongs_to :table
+	belongs_to :user
+	belongs_to :table
+
+	validates :start_time, presence: true 
+    validates :duration, presence: true, numericality: { greater_than_or_equal_to: 10, :less_than_or_equal_to => 60 }
 end
